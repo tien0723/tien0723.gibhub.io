314a358105dfaa007528b302113a4b976c9ac4c0
tien07230 <tien07230@gmail.com>
2022-12-14 16:58:53 +0700

    14/12/2022

--- js/script.js
+++ js/script.js
@@ -1,58 +1,66 @@
 
-				
+
 $(".slideshow").slick({
 
   infinite: true,
   autoplay: false,
   arrows: true,
   autoplaySpeed: 2000,
-  prevArrow:"<span class='arrow prev'><i class='fas fa-angle-left'></i></span>",
- 	nextArrow:"<span class='arrow next'><i class='fas fa-angle-right'></i></span>",
+  prevArrow: "<span class='arrow prev'><i class='fas fa-angle-left'></i></span>",
+  nextArrow: "<span class='arrow next'><i class='fas fa-angle-right'></i></span>",
 
   //  toc do chieu dai khong khoang cach
   speed: 1000,
   slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 995,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
   // centerMode: true,
   // variableWidth: true
 });
 
 
-const $active =document.querySelector('.tab-item.active')
-const $activeProduct =document.querySelector('.tab-product.active')
-const $A =document.querySelector.bind(document)
-const tabs =document.querySelectorAll('.tab-item')
-const tabsProduct =document.querySelectorAll('.tab-product')
+const $active = document.querySelector('.tab-item.active')
+const $activeProduct = document.querySelector('.tab-product.active')
+const $A = document.querySelector.bind(document)
+const tabs = document.querySelectorAll('.tab-item')
+const tabsProduct = document.querySelectorAll('.tab-product')
 
 const $line1 = document.querySelector('.product .tabs .line')
-console.log( $line1)
+console.log($line1)
 $line1.style.left = $active.offsetLeft + "px"
 $line1.style.width = $active.offsetWidth + "px"
 
-tabs.forEach((tab,index) => {
+tabs.forEach((tab, index) => {
   const product = tabsProduct[index]
-  
-  tab.onclick =function (){
+
+  tab.onclick = function () {
     // console.log(product)
-        $A('.tab-item.active').classList.remove('active')
-        $A('.tab-product.active').classList.remove('active')
-         console.log(product)
-         tab.classList.add('active')
-        product.classList.add('active')
-         $line1.style.left = tab.offsetLeft + "px"
-         $line1.style.width = tab.offsetWidth + "px"
-         console.log( $line1)
+    $A('.tab-item.active').classList.remove('active')
+    $A('.tab-product.active').classList.remove('active')
+    console.log(product)
+    tab.classList.add('active')
+    product.classList.add('active')
+    $line1.style.left = tab.offsetLeft + "px"
+    $line1.style.width = tab.offsetWidth + "px"
+    console.log($line1)
   }
 });
 //header----------------------------------------------------------------
-$(document).ready(function(){
-  $(window).scroll(function(){
+$(document).ready(function () {
+  $(window).scroll(function () {
     console.log($(this).scrollTop())
-   if($(this).scrollTop()){
-       $('header').addClass('sticky');
-       console.log("asdasdasda")
-   }else{
-       $('header').removeClass('sticky');
-   }
+    if ($(this).scrollTop()) {
+      $('header').addClass('sticky');
+      console.log("asdasdasda")
+    } else {
+      $('header').removeClass('sticky');
+    }
   })
 })
 
